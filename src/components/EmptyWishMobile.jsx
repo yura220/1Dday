1fe737784ab1dc62d7203d52120e1d2117351550
yura220 <ruddbfk7@gmail.com>
2025-07-16 13:31:13 +0900

    업로드2

--- src/components/EmptyWishMobile.jsx
+++ src/components/EmptyWishMobile.jsx
@@ -4,7 +4,7 @@ import CardWishBest from './CardWishBest';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import '../assets/css/WishListPage.css';
+import '../assets/css/wishListPage.css';
 
 export default function EmptyWish() {
   const [bestProducts, setBestProducts] = useState([]);
